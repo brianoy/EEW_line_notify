8ae3416ef2b896776bf8f93ea8b25f956bccbfa3
brianoy <brian20020925@gmail.com>
2024-04-24 19:00:39 +0800

    Add files via upload

--- line_notify.py
+++ line_notify.py
@@ -1,18 +1,24 @@
 import requests
 import sys
 lineUrl = "https://notify-api.line.me/api/notify"
-lineToken = "<line notify token>"
+lineToken = "zFy9BEfHyFxkRj9fqzXPb7Xja2UTU9s3PAdHtzZLNYc"
 
-magnitude = str(sys.argv[1]).replace("+","強").replace("-","弱")
-second = int(sys.argv[2])
+try:
+    magnitude = str(sys.argv[1]).replace("+","強").replace("-","弱")
+    second = int(sys.argv[2])
 
-def lineWebhook(msg,token)->None:
-    headers = { "Authorization": "Bearer " + token }
-    data = { 'message': msg }
-    requests.post(lineUrl,headers = headers, data = data)
+    def lineWebhook(msg,token)->None:
+        headers = { "Authorization": "Bearer " + token }
+        data = { 'message': msg }
+        requests.post(lineUrl,headers = headers, data = data)
 
-msg = "警告：地區預計震度" + magnitude + "級地震\n預計到達時間:" + second + "秒"
-
-lineWebhook(msg,lineToken)
-
+    msg = "警告：地區預計震度" + str(magnitude) + "級地震\n預計到達時間:" + str(second) + "秒"
+    
+    lineWebhook(msg,lineToken)
+    
+except ModuleNotFoundError:
+    print("=====錯誤:請確保request和sys module有被正確的安裝=====")
+    
+except IndexError:
+    print("=====錯誤:請確保呼叫此程式時參數有正確的引數(規模和秒數)=====")
 
